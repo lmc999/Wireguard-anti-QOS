4692861d848ac278ed59d5f06937910037d42b1e
lmc999 <39726914+lmc999@users.noreply.github.com>
2018-11-23 00:20:06 +0800

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d %~dp0
+%1 start mshta vbscript:createobject("wscript.shell").run("""%~0"" ::",0)(window.close)&&exit
+
+start /b D:\software\TunSafe\udp\udp2raw.exe -c -r44.55.66.77:9898 -l 127.0.0.1:9999 --raw-mode faketcp -k passwd 
+
+
+
